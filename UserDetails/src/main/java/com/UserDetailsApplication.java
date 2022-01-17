a3b4a394da90592404b234d3da1c6b58ee7a6566
Neha Sahu <nehasahu9424@gmail.com>
2022-01-17 18:43:51 +0530

    deploye the app to the external server

--- UserDetails/src/main/java/com/UserDetailsApplication.java
+++ UserDetails/src/main/java/com/UserDetailsApplication.java
@@ -2,13 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.security.AppProperties;
 
 @SpringBootApplication
-public class UserDetailsApplication {
+public class UserDetailsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserDetailsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserDetailsApplication.class, args);
